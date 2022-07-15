d87ef3569d735b9431f420d429ba0c9286a8eacd
Andrew Leung <andrewleung104@gmail.com>
2022-07-15 08:26:42 -0400

    Implement POC client-side oath flow
    This just proves that the Freesound OAuth flow works. However, I've decided that this project will need a backend where this flow should live. There are too many tight couplings right now that I'd rather not deal with.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
-function App() {
-  return <div className="App">Hello World</div>;
-}
+import useFreesoundAccessToken from '../hooks/useFreesoundAccessToken';
+import { Button } from '@chakra-ui/react';
+
+// TODO: Get rid of these... they should be server-side...
+const CLIENT_ID = import.meta.env.VITE_FREESOUND_CLIENT_ID;
+const CLIENT_SECRET = import.meta.env.VITE_FREESOUND_CLIENT_SECRET;
+
+console.log(CLIENT_ID);
+
+const App = () => {
+  const maybeToken = useFreesoundAccessToken(CLIENT_ID, CLIENT_SECRET);
+
+  if (maybeToken.isNothing()) {
+    return (
+      <Button
+        onClick={() => {
+          window.location.replace(
+            `https://freesound.org/apiv2/oauth2/authorize/?client_id=${CLIENT_ID}&response_type=code`,
+          );
+        }}
+      >
+        Login with Freesound
+      </Button>
+    );
+  }
+
+  return <div className="App">hello</div>;
+};
 
 export default App;
