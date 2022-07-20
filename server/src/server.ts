37a1f08cc14fa074258f4182488602eb984846e9
Andrew Leung <andrewleung104@gmail.com>
2022-07-20 12:28:00 -0400

    Register fastifyJwt; add AES encryption functions
    The plan is to encrypt the access and refresh tokens and supply them in a JWT. This allows Raudi to have stateless auth while never exposing Freesound tokens.
    
    Raudi will only accept a full JWT, and is protected from XSS since there is no room whatsoever for code injection. CSRF attacks are also protected against since the JWT will be stored in a `SameSite` cookie.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,37 @@
+import crypto from 'crypto';
 import Fastify from 'fastify';
+import fastifyJwt from '@fastify/jwt';
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import { AuthCode } from './schemas/auth.js';
 import 'dotenv/config';
 
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+const encrypt = (text: crypto.BinaryLike): string => {
+  const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+  const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+  return encrypted.toString('hex');
+};
+
+const decrypt = (encryptedData: string): string => {
+  const encryptedText = Buffer.from(encryptedData, 'hex');
+  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+  const decrypted = Buffer.concat([
+    decipher.update(encryptedText),
+    decipher.final(),
+  ]);
+  return decrypted.toString();
+};
+
 const fastify = Fastify({
   logger: true,
 }).withTypeProvider<TypeBoxTypeProvider>();
+
+fastify.register(fastifyJwt, {
+  secret: crypto.randomBytes(256).toString('base64'),
+});
 
 fastify.get(
   '/token',
@@ -16,6 +42,8 @@ fastify.get(
   },
   async (request, reply) => {
     const { code } = request.query;
+
+    // Encrypt the code.
 
     reply.send(code);
   },
