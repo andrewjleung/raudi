152148e2a35c9caca354735b3e32fec919301689
Andrew Leung <andrewleung104@gmail.com>
2022-07-27 22:35:19 -0400

    Refactor Freesound API; add `getSound` endpoint
    - Get rid of `objectToParams`, `got` already lets you specify search params via options.
    - Handle exceptions in `getRandomSoundId` with `Either`

--- server/src/apis/freesound.ts
+++ server/src/apis/freesound.ts
@@ -1,16 +1,10 @@
-import { Maybe, Nothing } from 'purify-ts';
+import { Either, Left, Maybe, Nothing, Right } from 'purify-ts';
 import got from 'got';
-import { AccessTokenResponse } from '../types.js';
+import { AccessTokenResponse, SoundInstance } from '../types.js';
 import { config } from '../config.js';
 
 const FREESOUND_URL = 'https://freesound.org';
 const FREESOUND_API_URL = `${FREESOUND_URL}/apiv2`;
-
-// TODO: Test this and move it
-const objectToParams = (o: Record<string, string>): string =>
-  Object.entries(o)
-    .map(([key, val]) => `${key}=${val}`)
-    .join('&');
 
 const getAccessToken = async (
   authCode: string,
@@ -19,18 +13,17 @@ const getAccessToken = async (
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
     },
+    searchParams: {
+      client_id: config.freesoundClientId,
+      client_secret: config.freesoundClientSecret,
+      grant_type: 'authorization_code',
+      code: authCode,
+    },
   };
-
-  const params = objectToParams({
-    client_id: config.freesoundClientId,
-    client_secret: config.freesoundClientSecret,
-    grant_type: 'authorization_code',
-    code: authCode,
-  });
 
   try {
     const response = await got
-      .post(`${FREESOUND_API_URL}/oauth2/access_token?${params}`, options)
+      .post(`${FREESOUND_API_URL}/oauth2/access_token`, options)
       .json();
 
     return AccessTokenResponse.decode(response).toMaybe();
@@ -41,13 +34,42 @@ const getAccessToken = async (
   }
 };
 
-const getRandomSoundId = async (): Promise<string> => {
-  const response = await got.get(`${FREESOUND_URL}/browse/random`);
-  const responseUrl = new URL(response.url);
-  const locations = responseUrl.pathname.split('/');
-  const soundId = locations[locations.indexOf('sounds') + 1];
+const getRandomSoundId = async (): Promise<Either<unknown, string>> => {
+  try {
+    const response = await got.get(`${FREESOUND_URL}/browse/random`);
+    const responseUrl = new URL(response.url);
+    const locations = responseUrl.pathname.split('/');
+    const soundId = locations[locations.indexOf('sounds') + 1];
 
-  return soundId;
+    return Right(soundId);
+  } catch (e) {
+    // TODO: DRY this?
+    return Left(e);
+  }
 };
 
-export { getAccessToken, getRandomSoundId };
+const getSound = async (
+  id: string,
+): Promise<Either<unknown, SoundInstance>> => {
+  // Filter fields by those specified in the SoundInstance type.
+  const fields = Object.keys(SoundInstance.schema().properties);
+
+  const options = {
+    searchParams: {
+      fields: fields.join(','),
+    },
+  };
+
+  try {
+    const response = await got.post(
+      `${FREESOUND_API_URL}/sounds/${id}`,
+      options,
+    );
+
+    return SoundInstance.decode(response);
+  } catch (e) {
+    return Left(e);
+  }
+};
+
+export { getAccessToken, getRandomSoundId, getSound };
