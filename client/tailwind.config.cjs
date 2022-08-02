6bd78f6055de372491008d9df00691a216d3a6c3
Andrew Leung <andrewleung104@gmail.com>
2022-08-02 12:15:37 -0400

    Move components/hooks dirs into `src`

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js,jsx,ts,tsx}',
-    './components/**/*.{html,js,jsx,ts,tsx}',
-  ],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
