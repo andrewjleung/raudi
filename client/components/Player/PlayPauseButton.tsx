6dd8d819dafac3eb937c3fd5a4a20a5b2a0618f5
Andrew Leung <andrewleung104@gmail.com>
2022-08-06 20:21:41 -0400

    Remove accidental duplicated component

--- client/components/Player/PlayPauseButton.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@chakra-ui/react";
-
-export default ({ playing, setPlaying }) => (
-  <Button aria-label="Play/pause button" onClick={() => setPlaying(!playing)}>
-    {playing ? "Pause" : "Play"}
-  </Button>
-);
