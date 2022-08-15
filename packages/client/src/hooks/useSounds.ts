f68b45c61fb651f19a319b5531a76ebc66e23c4e
Andrew Leung <andrewleung104@gmail.com>
2022-08-15 11:32:17 -0400

    Fix `useSounds` hook retry; reset sound loc on logout
    The `useSounds` hook should be disabled as soon as the user gets logged out so that it doesn't keep retrying after an unauthorized response.
    
    The sound location should also be reset on logout so that when the user logs back in, it'll be in sync with the data.

--- packages/client/src/hooks/useSounds.ts
+++ packages/client/src/hooks/useSounds.ts
@@ -1,11 +1,11 @@
 import { FreesoundSoundInstance } from '@raudi/types';
 import { Just, Maybe, Nothing } from 'purify-ts';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchSounds } from '../api/soundsApi';
 import { useAuthorizedFetch } from './useAuthorizedFetch';
 import { useInfiniteQuery, InfiniteData } from '@tanstack/react-query';
+import useLogin from './useLogin';
 
-// TODO: pages may vary in size...
 const STAYAHEAD_PAGE_COUNT = 3;
 
 export type UseSounds = {
@@ -18,6 +18,11 @@ export type UseSounds = {
 type SoundLocation = {
   page: number;
   soundIndex: number;
+};
+
+const DEFAULT_SOUND_LOCATION: SoundLocation = {
+  page: 0,
+  soundIndex: 0,
 };
 
 const getNextSoundLocation = (
@@ -45,25 +50,26 @@ const getNextSoundLocation = (
 // TODO: Cleanup sounds after a certain threshold, currently they will keep
 //       building up forever.
 export const useSounds = (): UseSounds => {
-  const [soundLocation, setSoundLocation] = useState<SoundLocation>({
-    page: 0,
-    soundIndex: 0,
-  });
+  const [soundLocation, setSoundLocation] = useState<SoundLocation>(
+    DEFAULT_SOUND_LOCATION,
+  );
   const authorizedFetch = useAuthorizedFetch();
+  const { isLoggedIn } = useLogin();
 
   // TODO: Handle errors?
   const { data, error, fetchNextPage, isFetching, isFetchingNextPage } =
     useInfiniteQuery(
-      ['sounds'],
+      ['sounds', isLoggedIn],
       () => fetchSounds(authorizedFetch).then((value) => value.unsafeCoerce()),
       {
         refetchOnWindowFocus: false,
-        // Random sound data is not inherently paginated, so no pagination params
-        // are really necessary here. However, we don't want `react-query` to wipe
-        // previous data with each fetch, so just return true here rather than any
-        // params.
-        getNextPageParam: () => true,
+        // Random sound data is not inherently paginated, so no pagination
+        // params are really necessary here. However, we don't want
+        // `react-query` to wipe previous data with each fetch, so just return
+        // true here rather than any params.
+        getNextPageParam: () => isLoggedIn,
         cacheTime: 0,
+        enabled: isLoggedIn,
       },
     );
 
@@ -75,10 +81,16 @@ export const useSounds = (): UseSounds => {
       data.pages.length - soundLocation.page >= STAYAHEAD_PAGE_COUNT;
 
     if (!isFetchingNextPage && !isAhead()) {
-      console.log('fetching next page.');
       fetchNextPage();
     }
   };
+
+  // Reset the sound location when the user gets logged out.
+  useEffect(() => {
+    if (!isLoggedIn) {
+      setSoundLocation(DEFAULT_SOUND_LOCATION);
+    }
+  }, [isLoggedIn]);
 
   return {
     sound: data
