918bca593e6413425b8ab271ed758808ab48e522
Andrew Leung <andrewleung104@gmail.com>
2022-08-15 13:31:11 -0400

    Create play/pause button with fontawesome

--- packages/client/src/components/Player/PlayPauseButton.tsx
+++ packages/client/src/components/Player/PlayPauseButton.tsx
@@ -1,18 +1,35 @@
 import { State } from '../../types';
-import { PlayIcon, PauseIcon } from '@heroicons/react/solid';
-import CircularButton from '../CircularButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
 
 type PlayPauseButtonProps = {
   Playing: State<boolean>;
+};
+
+const PlayPauseIcon = ({ playing }: { playing: boolean }) => {
+  if (!playing) {
+    return (
+      <FontAwesomeIcon
+        icon={faPlay}
+        size="lg"
+        color="white"
+        className="ml-0.5"
+      />
+    );
+  }
+
+  return <FontAwesomeIcon icon={faPause} size="lg" color="white" />;
 };
 
 export default function PlayPauseButton({ Playing }: PlayPauseButtonProps) {
   const [playing, setPlaying] = Playing;
 
   return (
-    <CircularButton
-      icon={playing ? <PauseIcon /> : <PlayIcon />}
+    <div
       onClick={() => setPlaying((playing) => !playing)}
-    />
+      className="rounded-full w-10 h-10 bg-black ease-in-out duration-75 hover:scale-105 active:scale-110 flex justify-center items-center"
+    >
+      <PlayPauseIcon playing={playing} />
+    </div>
   );
 }
