13813ac365a0b09286230a1ea5e6867c4691bc2c
Andrew Leung <andrewleung104@gmail.com>
2022-08-15 11:04:37 -0400

    Use array codec for decoding sound data

--- packages/client/src/api/soundsApi.ts
+++ packages/client/src/api/soundsApi.ts
@@ -1,21 +1,12 @@
 import { FreesoundSoundInstance } from '@raudi/types';
-import { Either, Left } from 'purify-ts';
+import { array, Either, EitherAsync } from 'purify-ts';
 import { AuthorizedFetch } from '../hooks/useAuthorizedFetch';
 
 export const fetchSounds = async (
   authorizedFetch: AuthorizedFetch,
-): Promise<Either<unknown, FreesoundSoundInstance[]>> => {
-  try {
-    const response = await authorizedFetch(
-      'http://localhost:3000/sounds/random',
-    ).then((response) => response.json());
-
-    if (!Array.isArray(response)) {
-      return Left(Error('Random sounds response is not an array.'));
-    }
-
-    return Either.sequence(response.map(FreesoundSoundInstance.decode));
-  } catch (e) {
-    return Left(e);
-  }
-};
+): Promise<Either<unknown, FreesoundSoundInstance[]>> =>
+  EitherAsync.fromPromise(() =>
+    authorizedFetch('http://localhost:3000/sounds/random'),
+  ).chain((response) =>
+    EitherAsync.liftEither(array(FreesoundSoundInstance).decode(response)),
+  );
