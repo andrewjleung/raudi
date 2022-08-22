53f3c9a532aa5c3069d556eba565340974f70dab
Andrew Leung <andrewleung104@gmail.com>
2022-08-22 17:14:32 -0400

    Navigate home when clicking "raudi"

--- packages/client/src/components/NavBar/index.tsx
+++ packages/client/src/components/NavBar/index.tsx
@@ -1,4 +1,5 @@
 import { Container, Heading } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 import NavButton from './NavButton';
 
 type NavBarProps = {
@@ -6,10 +7,12 @@ type NavBarProps = {
 };
 
 export default function NavBar({ children }: NavBarProps) {
+  const navigate = useNavigate();
+
   return (
     <Container>
       <div className="h-20 sticky top-0 flex flex-row items-center gap-1 bg-white border-b-1">
-        <div className="grow">
+        <div className="grow cursor-pointer" onClick={() => navigate('/')}>
           <Heading textColor="red.300">raudi</Heading>
         </div>
         <NavButton href="/about">About</NavButton>
