848556df9fc7a9c3678cbfd722d3fa18e30cf3b2
Andrew Leung <andrewleung104@gmail.com>
2022-09-25 20:31:35 -0400

    Don't enforce port env vars

--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -6,13 +6,13 @@ export type RaudiClientConfig = {
 } & RaudiBaseConfig;
 
 const buildConfig = (): RaudiClientConfig => {
-  if (import.meta.env?.VITE_CLIENT_PORT === undefined) {
-    throw new Error('Missing client port from configuration.');
-  }
+  // if (import.meta.env?.VITE_CLIENT_PORT === undefined) {
+  //   throw new Error('Missing client port from configuration.');
+  // }
 
-  if (import.meta.env?.VITE_SERVER_PORT === undefined) {
-    throw new Error('Missing server port from configuration');
-  }
+  // if (import.meta.env?.VITE_SERVER_PORT === undefined) {
+  //   throw new Error('Missing server port from configuration');
+  // }
 
   const clientPort = import.meta.env.VITE_CLIENT_PORT;
   const serverPort = import.meta.env.VITE_SERVER_PORT;
