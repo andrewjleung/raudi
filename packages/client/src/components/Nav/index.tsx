0613fe9a9ee2934b303628a33b493e0b960007d6
Andrew Leung <andrewleung104@gmail.com>
2022-11-11 22:51:08 -0500

    Separate nav drawer into separate component

--- packages/client/src/components/Nav/index.tsx
+++ packages/client/src/components/Nav/index.tsx
@@ -1,12 +1,85 @@
-import { Heading, useDisclosure } from '@chakra-ui/react';
+import { Heading, useDisclosure, UseDisclosureReturn } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import NavButton from './NavButton';
 import { Squash as Hamburger } from 'hamburger-react';
 import cn from 'classnames';
 import ToolCardContent from '../ToolCardContent';
 
+type DrawerProps = {
+  Disclosure: UseDisclosureReturn;
+};
+
+const Drawer = ({ Disclosure }: DrawerProps) => {
+  const { isOpen, onClose } = Disclosure;
+
+  return (
+    <div
+      id="drawer"
+      className={cn(
+        'fixed',
+        'overflow-x-hidden',
+        'right-0',
+        'px-10',
+        'transition-height ease-in-out',
+        isOpen ? 'duration-700' : '',
+        'bg-white',
+        'w-full',
+        isOpen ? 'h-screen' : 'h-0',
+        'rounded-lg',
+        'overflow-hidden',
+      )}
+    >
+      <div
+        className={cn(
+          'p-4',
+          'transition-opacity ease-in-out',
+          isOpen ? 'duration-200 delay-500' : '',
+          isOpen ? 'opacity-100' : 'opacity-0',
+          'grid grid-cols-2 gap-4',
+        )}
+      >
+        <NavButton
+          className="col-span-2 sm:col-span-1"
+          href="/sounds"
+          onClick={onClose}
+        >
+          <ToolCardContent
+            title="Random Sounds"
+            description="Demo and download random soundbytes for instant inspiration, Tinder-style."
+          />
+        </NavButton>
+        <NavButton
+          className="col-span-2 sm:col-span-1"
+          href="/genres"
+          onClick={onClose}
+        >
+          <ToolCardContent
+            title="Random Genres"
+            description="Pick a random genre from Anthony Fantano's reviews to make music to."
+          />
+        </NavButton>
+        <NavButton
+          className="visible sm:invisible"
+          href="/about"
+          onClick={onClose}
+        >
+          About
+        </NavButton>
+        <NavButton
+          className="visible sm:invisible"
+          href="/contact"
+          onClick={onClose}
+        >
+          Contact
+        </NavButton>
+      </div>
+    </div>
+  );
+};
+
 export default function NavBar() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const Disclosure = useDisclosure();
+  const { isOpen, onOpen, onClose } = Disclosure;
   const navigate = useNavigate();
 
   const toggle = () => {
@@ -56,67 +129,7 @@ export default function NavBar() {
           rounded
         />
       </div>
-      <div
-        id="drawer"
-        className={cn(
-          'fixed',
-          'overflow-x-hidden',
-          'right-0',
-          'px-10',
-          'transition-height ease-in-out',
-          isOpen ? 'duration-700' : '',
-          'bg-white',
-          'w-full',
-          isOpen ? 'h-screen' : 'h-0',
-          'rounded-lg',
-          'overflow-hidden',
-        )}
-      >
-        <div
-          className={cn(
-            'p-4',
-            'transition-opacity ease-in-out',
-            isOpen ? 'duration-200 delay-500' : '',
-            isOpen ? 'opacity-100' : 'opacity-0',
-            'grid grid-cols-2 gap-4',
-          )}
-        >
-          <NavButton
-            className="col-span-2 sm:col-span-1"
-            href="/sounds"
-            onClick={onClose}
-          >
-            <ToolCardContent
-              title="Random Sounds"
-              description="Demo and download random soundbytes for instant inspiration, Tinder-style."
-            />
-          </NavButton>
-          <NavButton
-            className="col-span-2 sm:col-span-1"
-            href="/genres"
-            onClick={onClose}
-          >
-            <ToolCardContent
-              title="Random Genres"
-              description="Pick a random genre from Anthony Fantano's reviews to make music to."
-            />
-          </NavButton>
-          <NavButton
-            className="visible sm:invisible"
-            href="/about"
-            onClick={onClose}
-          >
-            About
-          </NavButton>
-          <NavButton
-            className="visible sm:invisible"
-            href="/contact"
-            onClick={onClose}
-          >
-            Contact
-          </NavButton>
-        </div>
-      </div>
+      <Drawer Disclosure={Disclosure} />
     </div>
   );
 }
