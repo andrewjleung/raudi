5cf775fcbdbd55934b014463a884c96e0d24bc63
Andrew Leung <andrewleung104@gmail.com>
2022-11-12 02:54:15 -0500

    Style the genres page

--- packages/client/src/routes/Genres.tsx
+++ packages/client/src/routes/Genres.tsx
@@ -1,6 +1,25 @@
-import { Button, CircularProgress } from '@chakra-ui/react';
+import { CircularProgress } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import { fetchRandomGenre } from '../api/raudiApi';
+
+const PROMPTS: string[] = [
+  "Why don't you try making some $ music?",
+  'How about $?',
+  "I really think you'd make a good $ track.",
+  '$ is in.',
+  'Next stop, $.',
+  'You and $. A match made in heaven.',
+  'What even is $?',
+  "You're long overdue for flexing those $ chops.",
+  "Let's keep things simple. Up for some $?",
+  'I heard a really great $ track the other day...',
+  "C'mon... I know you're itching to make some $.",
+  'This is gonna sound weird but... what about $?',
+  'Please bless our ears with some $.',
+  'Take a shot at some $.',
+  "I'd like to see you do your best $ impression.",
+  "$? Isn't that what Radiohead does?",
+];
 
 export default function Genres() {
   const { data, error, isLoading, refetch } = useQuery(
@@ -11,6 +30,9 @@ export default function Genres() {
     },
   );
 
+  const [before, after] =
+    PROMPTS[Math.floor(Math.random() * PROMPTS.length)].split('$');
+
   if (data === undefined || isLoading) {
     return <CircularProgress />;
   }
@@ -20,9 +42,17 @@ export default function Genres() {
   }
 
   return (
-    <div>
-      <div>{data}</div>
-      <Button onClick={() => refetch()}>Get a new genre</Button>
+    <div className="flex flex-row gap-8 text-center justify-center items-center mt-20 sm:mt-32">
+      <div
+        className="text-4xl max-w-md cursor-pointer hover:scale-105 active:scale-100 transition ease-in-out duration-100 rounded-xl px-6 py-4 border-1 hover:bg-orange-100"
+        onClick={() => refetch()}
+      >
+        {before}
+        <span className="underline font-bold decoration-4 decoration-solid decoration-orange-400 text-orange-400">
+          {data.toUpperCase()}
+        </span>
+        {after}
+      </div>
     </div>
   );
 }
